167c0502765d8d032199e96bda3d7b07e6aa0e06
Raffaele Tosti <raffaele.tosti@gmail.com>
2025-11-24 15:36:25 +0100

    feat(admin.py): Admin support for document-tag many to many, version-aware contraints migration

--- src/django_simple_dms/admin.py
+++ src/django_simple_dms/admin.py
@@ -4,16 +4,21 @@ from django.contrib.admin.widgets import AdminDateWidget
 from django.contrib.postgres.fields import DateRangeField
 from django.contrib.postgres.forms import RangeWidget
 
-from django_simple_dms.models import Document, DocumentTag, TagGrant, DocumentGrant
+from django_simple_dms.models import Document, DocumentTag, TagGrant, DocumentGrant, Document2Tag
 
 
 class DocumentGrantInline(admin.TabularInline):
     model = DocumentGrant
 
 
+class Document2TagInline(admin.TabularInline):
+    model = Document2Tag
+    extra = 1
+
+
 @admin.register(Document)
 class DocumentAdmin(admin.ModelAdmin):
-    list_display = ('document', 'admin', 'upload_date', 'reference_period')
+    list_display = ('document', 'admin', 'upload_date', 'reference_period', 'tags_list')
 
     list_filter = (
         'admin',
@@ -21,12 +26,17 @@ class DocumentAdmin(admin.ModelAdmin):
     )
 
     search_fields = ('document', 'admin')
-    inlines = [DocumentGrantInline]
+    inlines = [DocumentGrantInline, Document2TagInline]
 
     formfield_overrides = {
         # Tell Django to use our custom widget for all DateRangeFields in this admin.
         DateRangeField: {'widget': RangeWidget(base_widget=AdminDateWidget)},
     }
+
+    def tags_list(self, obj: Document) -> str:
+        return ', '.join([tag.title for tag in obj.tags.all()])
+
+    tags_list.short_description = 'Tags'
 
 
 class TagGrantInline(admin.TabularInline):
